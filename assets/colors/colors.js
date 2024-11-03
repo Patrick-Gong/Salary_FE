f5d30c1615ba33366bb3ed4ec98904a84d9c1e96
yminjuu <ymj0580@naver.com>
2024-11-04 00:57:16 +0900

    chore: 공통 colors 객체 및 font 추가

---
+++ assets/colors/colors.js
@@ -0,0 +1,19 @@
+const colors = {
+  Grayscale_white: "#FFFFFF",
+  Grayscale_40: "#A0A0A0",
+  Grayscale_60: "#717171",
+  Grayscale_80: "#414141",
+  Grayscale_90: "#313131",
+  Grayscale_100: "#121212",
+  Success_100: "#4AD157",
+  Secondary_20: "#FBFFE4",
+  Secondary_60: "#C6CBB1",
+  Secondary_80: "#DEED90",
+  Secondary_100: "#C1F032",
+  Primary_20: "#F6FCD9",
+  Primary_80: "#C7EC00",
+  Primary_100: "#D7FF01",
+  Warning_100: "#F34B4B",
+};
+
+export default colors;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,32 @@
-import { Text, View } from 'react-native';
-import styled from 'styled-components/native';
+import { Text, View } from "react-native";
+import styled from "styled-components/native";
+import TodaySalaryScreen from "../screens/TodaySalaryScreen";
+import TodayTrendQuiz from "../screens/TodayTrendQuizScreen";
+import colors from "../assets/colors/colors";
+import { setCustomText } from "react-native-global-props";
 
 const ViewContainer = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  color: ${colors.Warning_100};
+  background-color: ${colors.Grayscale_Primary_100};
+`;
+
+const PrimaryText = styled.Text`
+  color: ${colors.Warning_100};
+  font-family: "Pretendard";
 `;
 
 function HomeScreen() {
   return (
     <ViewContainer>
-      <Text>This screen is HomeScreen</Text>
+      <PrimaryText>This screen is HomeScreen</PrimaryText>
+      {/* 날짜 관리와 스트립 형태의 달력 컴포넌트 */}
+      {/* 스텝과 학습 진행률 컴포넌트 */}
+      <TodaySalaryScreen />
+      <TodayTrendQuiz />
+      {/* 아티클 */}
     </ViewContainer>
   );
 }
